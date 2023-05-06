89550f93936f131e7010ca40212ff67b7ce68e6d
Rahul Rajesh <rahulrajeshrr365@gmail.com>
2023-05-06 16:38:28 +0000

    app -v1 finish

--- src/app/weather-service/weather-api-service.service.ts
+++ src/app/weather-service/weather-api-service.service.ts
@@ -2,18 +2,16 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { WeatherInterface } from '../model/climate-interface';
+import { environment } from 'src/environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherApiServiceService {
 
-  WEATHER_API_BASE_URL ='https://api.openweathermap.org/data/2.5';
-  WEATHER_API_KEY = 'f9c023d383bba1a696ca8371629d9b4e';
-
   constructor( private http: HttpClient ) { }
 
   getWeatherData(city: string): Observable<WeatherInterface>{
-    return this.http.get<WeatherInterface>(`${this.WEATHER_API_BASE_URL}/weather/?q=${city}&appid=${this.WEATHER_API_KEY}&units=metric`);
+    return this.http.get<WeatherInterface>(`${environment.WEATHER_API_BASE_URL}/weather/?q=${city}&appid=${environment.WEATHER_API_KEY}&units=metric`);
   }
 }
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,4 @@
-export const environment = {};
+export const environment = {
+    WEATHER_API_BASE_URL: 'https://api.openweathermap.org/data/2.5',
+    WEATHER_API_KEY: ' ',
+};
