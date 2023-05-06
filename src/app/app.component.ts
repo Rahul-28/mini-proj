c3460ef07b81e8bf30742a85f08cde2156ef6169
Rahul Rajesh <rahulrajeshrr365@gmail.com>
2023-05-06 12:31:33 +0000

    complete implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherApiServiceService } from './weather-service/weather-api-service.service';
+import { WeatherInterface } from './model/climate-interface';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  constructor(private weatheservice: WeatherApiServiceService ) {}
+
+  cityName: string = 'madurai';
+  weatherData?: WeatherInterface;
+
+  ngOnInit(): void {
+    this.retrieveWeatherData(this.cityName);
+  }
+
+  onSubmit() {
+    this.retrieveWeatherData(this.cityName);
+    this.cityName = '';
+  }
   
+  private retrieveWeatherData(cityName: string) {
+    this.weatheservice.getWeatherData(cityName)
+    .subscribe({
+      next: (response) => {
+        this.weatherData = response;
+      }
+    })
+  }
 }
